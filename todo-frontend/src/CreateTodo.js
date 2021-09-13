b8e67d61d6d4dd2c328c5029eeadeff0ef384ee7
Shaunak-B <shaunakbhatlawande@gmail.com>
2021-09-13 13:08:49 -0700

    todo complete functionality

--- todo-frontend/src/CreateTodo.js
+++ todo-frontend/src/CreateTodo.js
@@ -1,22 +1,64 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import "./CreateTodo.css"
+import swal from "sweetalert"
+import ToDoList from "./ToDoList"
 
 function CreateTodo() {
-    return (
-        <div className = "box">
-            <div className = "textEnd"> 
-                <h2>
-                    React TODO APP
-                </h2>
-                <p> Add a new TODO</p>
-            </div>
-            <div className = "addAction">
-                <input type="text" name = "ToDo" placeholder = "Write here..."/>
-                <button className = "toDoButton" type="button" name="addToDo">Add ToDo</button>  
-            </div>
+
+    const [todo, setTodo] = useState({title: "", done: false})
+    const [todoArr, setTodoArr] = useState([])
+    let todos = localStorage.hasOwnProperty("todos") ? JSON.parse(localStorage.getItem("todos")): []
+    useEffect(() => {
+    }, [todoArr])
+
+    const onChange = (event) => {
+        let {value} = event.target
+        let obj = {}
+        obj["title"] = value
+        obj["done"] = false
+        setTodo(obj)
+    }
+
+    const createTodo = (event) =>{
+        const {name} = event.target
+        if(event.key === "Enter" || name === "addToDo"){
+            if(todo.title !== ""){
+                todos.unshift(todo)
+                localStorage.setItem('todos', JSON.stringify(todos))
+                setTodo({title:"", done: false})
+            }else{
+                swal("Oops", "Please write ToDo first","error");
+            }
             
-        </div>
-    )
+        }
+    }
+
+    const completeTodo = (i) => {
+        if(todos[i]["done"] !== true){
+            todos[i]["done"] = true
+            localStorage.setItem("todos", JSON.stringify(todos))
+            setTodoArr(todos)
+            swal("Good Job", "Todo Completed","success");
+        }
+    }
+
+    return (
+       <> 
+            <div className = "box">
+                <div className = "textEnd"> 
+                    <h2>
+                        React TODO APP
+                    </h2>
+                    <p> Add a new TODO</p>
+                </div>
+                <div className = "addAction">
+                    <input type="text" name = "ToDo" placeholder = "Write here..." value = {todo.title} onKeyPress = {createTodo} onChange = {onChange}/>
+                    <button className = "toDoButton" type="button" name="addToDo" onClick={createTodo}>Add ToDo</button>  
+                </div>
+            </div>
+            <ToDoList todoArr = {todoArr} completeTodo={completeTodo}/>
+        </>    
+    );
 }
 
 export default CreateTodo
--- todo-frontend/src/ToDoList.js
+++ todo-frontend/src/ToDoList.js
@@ -4,43 +4,27 @@ import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 import IconButton from '@material-ui/core/IconButton';
 import CheckBoxIcon from '@material-ui/icons/CheckBox';
 
-function ToDoList() {
+function ToDoList(props)  {
+    let todoArr = props.todoArr === undefined ? [] : props.todoArr.length > 0 ? props.todoArr : JSON.parse(localStorage.getItem("todos"))
+    const {completeTodo} = props
+   
     return (
         <div className="list">
             <ul>
-                <li>
-                    <div>Hopping</div>
-                    <div className = "icon"> 
-                        <IconButton> 
-                            <CheckBoxIcon fontSize = 'large' className = "checkBoxButton"/>
-                        </IconButton>
-                        <IconButton> 
-                            <DeleteForeverIcon fontSize = 'large' className = "deleteButton"/>
-                        </IconButton> 
-                    </div>      
-                </li>
-                <li>
-                    <div>Walking</div>
-                    <div className = "icon"> 
-                        <IconButton> 
-                            <CheckBoxIcon fontSize = 'large' className = "checkBoxButton"/>
-                        </IconButton>
-                        <IconButton> 
-                            <DeleteForeverIcon fontSize = 'large' className = "deleteButton"/>
-                        </IconButton> 
-                    </div>      
-                </li>
-                <li>
-                    <div>running</div>
-                    <div className = "icon"> 
-                        <IconButton> 
-                            <CheckBoxIcon fontSize = 'large' className = "checkBoxButton"/>
-                        </IconButton>
-                        <IconButton> 
-                            <DeleteForeverIcon fontSize = 'large' className = "deleteButton"/>
-                        </IconButton> 
-                    </div>      
-                </li>
+                {todoArr && todoArr.length > 0 ? todoArr.map((el,i) =>(
+                    <li key={i}>
+                        <div className = {el["done"] ? "line-through": null}>{el.title}</div>
+                        <div className = "icon"> 
+                            <IconButton> 
+                                <CheckBoxIcon fontSize = 'large' onClick ={()=> completeTodo(i)} className ={`checkBoxButton ${el["done"] ? "green" : "blue"}`}  />
+                            </IconButton>
+                            <IconButton> 
+                                <DeleteForeverIcon fontSize = 'large' className = "deleteButton"/>
+                            </IconButton> 
+                        </div>      
+                    </li> 
+                )) : null
+                } 
             </ul>
         </div>
     )
